38c19c7ed5a7d7e8d4ada2c2281d747af5afcc04
MOHDSHEES <mohd.shees100@gmail.com>
2025-08-10 19:10:19 +0100

    bug dompurify

--- src/app/components/renderPurifyHtml.js
+++ src/app/components/renderPurifyHtml.js
@@ -1,7 +1,18 @@
-import DOMPurify from "dompurify";
+// import DOMPurify from "dompurify";
+
+// export const RenderSafeHTML = ({ html }) => {
+//   const cleanHTML = DOMPurify.sanitize(html);
+//   return (
+//     <p
+//       className="text-slate-400 mt-2"
+//       dangerouslySetInnerHTML={{ __html: cleanHTML }}
+//     />
+//   );
+// };
+import DOMPurify from "isomorphic-dompurify";
 
 export const RenderSafeHTML = ({ html }) => {
-  const cleanHTML = DOMPurify.sanitize(html);
+  const cleanHTML = DOMPurify.sanitize(html || "");
   return (
     <p
       className="text-slate-400 mt-2"
